3ba544a435de4fea1ab9ca41b7ab3fad1bb25a53
crownintldev <crownintltechnology@gmail.com>
2023-11-02 18:08:58 +0500

    selecting

--- src/components/Box/Box.tsx
+++ src/components/Box/Box.tsx
@@ -1,15 +1,69 @@
 "use client"
-import React, { useRef, useEffect } from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 
 const Box = ({ b }: { b: number }) => {
   const inputRefs = useRef<HTMLInputElement[]>([]);
+  const [selectionStart, setSelectionStart] = useState<number | null>(null);
+  const [selectionEnd, setSelectionEnd] = useState<number | null>(null);
+  const isSelecting = useRef(false);
+
+  const handleMouseDown = (index: number) => {
+    setSelectionStart(index);
+    setSelectionEnd(index);
+    isSelecting.current = true;
+  };
+
+  const handleMouseEnter = (index: number) => {
+    if (isSelecting.current) {
+      setSelectionEnd(index);
+    }
+  };
+
+  useEffect(() => {
+    const handleGlobalMouseUp = () => {
+      isSelecting.current = false;
+    };
+  
+    window.addEventListener('mouseup', handleGlobalMouseUp);
+    
+    return () => {
+      window.removeEventListener('mouseup', handleGlobalMouseUp);
+    };
+  }, []);
+
+  useEffect(() => {
+    const handleClickOutside = (event:any) => {
+      if (!inputRefs.current.some(ref => ref && ref.contains(event.target))) {
+        setSelectionStart(null);
+        setSelectionEnd(null);
+      }
+    };
+  
+    document.addEventListener('mousedown', handleClickOutside);
+  
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, []);
+
 
   const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>, index: number) => {
-    if (e.key === 'Backspace' && index > 0 && !inputRefs.current[index].value) {
+    if (e.key === 'Backspace' && selectionStart !== null && selectionEnd !== null && selectionStart !== selectionEnd) {
+      e.preventDefault();
+      const start = Math.min(selectionStart, selectionEnd);
+      const end = Math.max(selectionStart, selectionEnd);
+      for (let i = start; i <= end; i++) {
+        inputRefs.current[i].value = '';
+      }
+      setSelectionStart(null);
+      setSelectionEnd(null);
+      inputRefs.current[start].focus();
+    } else if (e.key === 'Backspace' && index > 0 && !inputRefs.current[index].value) {
       e.preventDefault();
       inputRefs.current[index - 1].focus();
       inputRefs.current[index - 1].value = '';
     }
+
     // Handle right arrow
     if (e.key === 'ArrowRight' && index < b - 1) {
       e.preventDefault();
@@ -22,7 +76,7 @@ const Box = ({ b }: { b: number }) => {
       inputRefs.current[index - 1].focus();
     }
   };
-// @ts-ignore
+  // @ts-ignore
   const distributeCharacters = (characters, startIndex) => {
     let nextIndex = startIndex;
     for (let char of characters) {
@@ -59,6 +113,19 @@ const Box = ({ b }: { b: number }) => {
     }
   }, [b]);
 
+  // for selection color
+  const isIndexSelected = (index: number) => {
+    // Convert to non-null values for comparison
+    let start = selectionStart ?? -1;
+    let end = selectionEnd ?? -1;
+  
+    // If selectionStart or selectionEnd is null, no selection is made
+    if (start === -1 || end === -1) return false;
+  
+    if (start > end) [start, end] = [end, start]; // Swap if start is greater than end
+    return index >= start && index <= end;
+  };
+
   return (
     <div className='flex'>
       {Array.from({ length: b }, (_, index) => (
@@ -66,7 +133,9 @@ const Box = ({ b }: { b: number }) => {
           key={index}
           // @ts-ignore
           ref={(el) => inputRefs.current[index] = el}
-          className='text-center border font-medium border-gray-800 d-block w-[16px] h-[16px]'
+          onMouseDown={() => handleMouseDown(index)}
+          onMouseEnter={() => handleMouseEnter(index)}
+          className={`text-center border font-medium border-gray-800 d-block w-[16px] h-[16px]  ${isIndexSelected(index) ? 'bg-blue-300' : ''}`}
           style={{ fontSize: "15px" }}
           type="text"
           onKeyDown={(e) => handleKeyDown(e, index)}
